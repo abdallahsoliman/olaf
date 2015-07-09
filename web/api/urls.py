9ec6d9bb4a604b60a419289839cdf72f3d1104ea
Abdallah Soliman <ass51@case.edu>
2015-07-09 18:49:14 -0400

    added users to api, need to test

--- web/api/urls.py
+++ web/api/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url, include
 from rest_framework import routers
+from api.views import UserViewSet
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
+router.register(r'users', UserViewSet)
