daf56650e7d2cfd60c9775d3bccc4538523bcc1d
Abdallah Soliman <abdallah.soliman@case.edu>
2015-11-14 16:37:21 -0500

    switched frontend over to Material Design Lite Framework by Google

--- web/dorm/urls.py
+++ web/dorm/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from dorm.views import *
 
 urlpatterns = [
-        url(r'^/$', DormView.as_view()),
+        url(r'^/$', DormView.as_view(), name="dorm"),
         url(r'actuators/$', ActuatorView.as_view()),
 
 ]
--- web/health/urls.py
+++ web/health/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from health.views import *
 
 urlpatterns = [
+        url(r'^/$', HeartRateView.as_view(), name='health'),
         url(r'heart-rate/$', HeartRateView.as_view()),
 ]
